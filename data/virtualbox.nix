766c27223ed570ac74d4c771e6bd2a1a7edcdbc7
Roxor-007 <koyande72@gmail.com>
2024-08-29 12:57:19 +0530

    Cleanup

--- data/virtualbox.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-  #virtualisation.virtualbox.guest.enable = true;
-  #virtualisation.virtualbox.guest.x11 = true;
-}
